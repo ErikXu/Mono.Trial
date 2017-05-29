c4d2296654379822df5215af8dff4ab921fc5ba1
Erik Xu <81636759@qq.com>
2017-05-29 12:53:10 +0800

    Add hello world.

---
+++ Mono.Trail/HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.Owin.Hosting;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (WebApp.Start<Startup>(ConfigurationManager.AppSettings["url"]))
+            {
+                Console.WriteLine("Server is running, Enter q to exit...");
+                while (true)
+                {
+                    if (Console.ReadLine() == "q")
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
